03f973a51f7f91e242c926cb92029e4589a86f66
findlerman09-lgtm <findlerman09@gmail.com>
2026-08-24 15:13:01 -0400

    Configure Formspree delivery endpoint

--- config.js
+++ config.js
@@ -3,11 +3,8 @@
    The Formspree form endpoint is intentionally public configuration, not a
    secret. Do not place Formspree API keys, email passwords, GitHub tokens, or
    other credentials in this repository.
-
-   Example endpoint shape:
-   https://formspree.io/f/xxxxxxxx
 */
 window.PSYCHPROFILE_CONFIG = Object.freeze({
   deliveryProvider: 'formspree',
-  formEndpoint: ''
+  formEndpoint: 'https://formspree.io/f/xppagqnp'
 });
